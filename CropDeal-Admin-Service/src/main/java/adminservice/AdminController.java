165fefe8b2c76ce621417eeab1295284330da4db
Navin <navin29rapelli@gmail.com>
2021-06-07 17:31:40 +0530

    first commit

---
+++ CropDeal-Admin-Service/src/main/java/adminservice/AdminController.java
@@ -0,0 +1,47 @@
+package adminservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+
+import adminserviceDAO.Farmer;
+
+@RestController
+@EnableHystrix
+public class AdminController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	
+	@GetMapping("/admin1")
+	@HystrixCommand(fallbackMethod = "fallback")
+	public  String demo1()
+	{
+		
+		Farmer far1=restTemplate.getForObject("http://CropDeal-Farmer-Service/farmer", Farmer.class) ;
+				
+				return "from admin"+far1;
+		
+		
+	}
+	
+	
+	private String fallback()
+	{
+		return "request fails";
+		
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ CropDeal-Admin-Service/src/test/java/adminservice/CropDealAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropDealAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
